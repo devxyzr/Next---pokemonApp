e7ab3410c5814dcd7a0509a3364813c8581b7244
devxyzr <develop.xyzr@gmail.com>
2023-01-18 21:41:08 -0500

    Take point a pokemon api

---
+++ src/pages/pokemon/[id].tsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import { Layout } from '../../components/layouts/Layout';
+
+interface Props {
+  pokemon: any;
+}
+
+const PokemonPage = () => {
+  const router = useRouter();
+  console.log(router.query);
+
+  return (
+    <Layout title="Algun pokemon">
+      <h1>Hola mundo</h1>
+    </Layout>
+  );
+};
+
+export default PokemonPage;
